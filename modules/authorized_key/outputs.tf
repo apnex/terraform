54a51a9119896dff44d4b0cc5ffd5dbf73549d6f
root <aobersnel@apnex.com.au>
2024-04-26 20:28:44 +1000

    Updates...

--- modules/authorized_key/outputs.tf
+++ modules/authorized_key/outputs.tf
@@ -2,3 +2,7 @@ output "private_key_pem" {
 	value		= tls_private_key.ssh.private_key_pem
 	sensitive	= true
 }
+output "private_key_openssh" {
+	value		= tls_private_key.ssh.private_key_openssh
+	sensitive	= true
+}
